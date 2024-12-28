e3dc27dfc201707f98c0b7bf030d2b4b3cf633af
SaraSaada <sara.saada218@gmail.com>
2024-12-28 23:00:45 +0200

    Update HW.c

--- HW.c
+++ HW.c
@@ -2,8 +2,32 @@
 
 int main() {
     int a, b;
+    char operation;
+
     printf("Enter two numbers: ");
     scanf("%d %d", &a, &b);
-    printf("The sum is: %d\n", a + b);
+
+    printf("Enter operation (+, -, *, /): ");
+    scanf(" %c", &operation);
+
+    switch (operation) {
+        case '+':
+            printf("The sum is: %d\n", a + b);
+            break;
+        case '-':
+            printf("The difference is: %d\n", a - b);
+            break;
+        case '*':
+            printf("The product is: %d\n", a * b);
+            break;
+        case '/':
+            if (b != 0)
+                printf("The division result is: %.2f\n", (float)a / b);
+            else
+                printf("Error: Division by zero!\n");
+            break;
+        default:
+            printf("Invalid operation.\n");
+    }
     return 0;
 }
